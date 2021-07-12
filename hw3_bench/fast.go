31719cb883679b89b8ad8b0409780245b3502ec6
kravcneger <kravcneger@gmail.com>
2021-07-13 00:59:37 +0400

    Add using easyjson in place of core json required performance 2782432 ns/op 559910 B/op 10422 allocs/op my performance 2642949 ns/op  522061 B/op  6787 allocs/op Mine one is better in all aspects.

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -1,46 +1,57 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
 	"regexp"
 	"strings"
+	"sync"
 )
 
+//easyjson:json
 type User struct {
-	Name     string   `json:"name"`  // will not be interned during unmarshaling
-	Email    string   `json:"email"` // will be interned during unmarshaling
+	Name     string   `json:"name"`
+	Email    string   `json:"email"`
 	Browsers []string `json:"browsers"`
 }
 
+var pool = sync.Pool{
+	// New creates an object when the pool has nothing available to return.
+	// New must return an interface{} to make it flexible. You have to cast
+	// your type after getting it.
+	New: func() interface{} {
+		// Pools often contain things like *bytes.Buffer, which are
+		// temporary and re-usable.
+		return &bytes.Buffer{}
+	},
+}
+
 // вам надо написать более быструю оптимальную этой функции
+
 func FastSearch(out io.Writer) {
-
 	file, err := os.Open(filePath)
-	if err != nil {
-		panic(err)
-	}
 
-	dec := json.NewDecoder(file)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
+	var foundUsers bytes.Buffer
+	scanner := bufio.NewScanner(file)
 
 	r := regexp.MustCompile("@")
 	seenBrowsers := []string{}
 	uniqueBrowsers := 0
-	foundUsers := ""
+	var user User
 
-	for i := 0; dec.More(); i++ {
+	for i := 0; scanner.Scan(); i++ {
 
 		isAndroid := false
 		isMSIE := false
 
-		user := User{}
-		err := dec.Decode(&user)
-
+		user.UnmarshalJSON(scanner.Bytes())
 		if err != nil {
 			panic(err)
 		}
@@ -86,11 +97,9 @@ func FastSearch(out io.Writer) {
 		if !(isAndroid && isMSIE) {
 			continue
 		}
-
-		email := r.ReplaceAllString(user.Email, " [at] ")
-		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
+		foundUsers.Write([]byte(fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, r.ReplaceAllString(user.Email, " [at] "))))
 	}
 
-	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "found users:\n"+foundUsers.String())
 	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
--- hw3_bench/go.mod
+++ hw3_bench/go.mod
@@ -1,3 +1,5 @@
 module bench/bench
 
 go 1.16
+
+require github.com/mailru/easyjson v0.7.7
